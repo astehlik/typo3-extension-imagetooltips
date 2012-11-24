2f903888b1432c601102d54f0afd5c2fb41c4dde
Alexander Stehlik <alexander.stehlik@googlemail.com>
2012-11-24 18:00:21 +0100

    Released version 0.1.0 to the TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "realurl".
-#
-# Auto generated 29-06-2011 10:48
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "imagetooltips".
+ *
+ * Auto generated 24-11-2012 17:58
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Image tooltips',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'fe',
 	'shy' => 0,
 	'version' => '0.0.0',
-	'dependencies' => '',
+	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -43,7 +43,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"f487";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.ext_update.php";s:4:"8518";s:20:"class.tx_realurl.php";s:4:"0155";s:29:"class.tx_realurl_advanced.php";s:4:"3efa";s:32:"class.tx_realurl_autoconfgen.php";s:4:"5226";s:28:"class.tx_realurl_tcemain.php";s:4:"93e1";s:21:"ext_conf_template.txt";s:4:"c890";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"e297";s:14:"ext_tables.php";s:4:"faed";s:14:"ext_tables.sql";s:4:"1b11";s:17:"locallang_csh.xml";s:4:"369d";s:16:"locallang_db.xml";s:4:"584a";s:12:"doc/TODO.txt";s:4:"b8cb";s:14:"doc/manual.sxw";s:4:"f0a2";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"0268";s:41:"modfunc1/class.tx_realurl_pagebrowser.php";s:4:"b616";s:22:"modfunc1/locallang.xml";s:4:"7e4f";}',
+	'_md5_values_when_last_written' => 'a:16:{s:12:"ext_icon.gif";s:4:"1456";s:17:"ext_localconf.php";s:4:"3314";s:14:"ext_tables.php";s:4:"66a0";s:14:"ext_tables.sql";s:4:"a932";s:25:"Classes/TooltipPlugin.php";s:4:"770e";s:21:"Configuration/Tca.php";s:4:"a3a4";s:38:"Configuration/TypoScript/constants.txt";s:4:"38a6";s:34:"Configuration/TypoScript/setup.txt";s:4:"29a0";s:42:"Configuration/TypoScript/4.5/constants.txt";s:4:"38a6";s:38:"Configuration/TypoScript/4.5/setup.txt";s:4:"e5b8";s:42:"Configuration/TypoScript/Example/setup.txt";s:4:"18e7";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"3737";s:32:"Resources/Public/Css/Example.css";s:4:"5fd9";s:52:"Resources/Public/JavaScript/TooltipInitialization.js";s:4:"c4cd";s:14:"doc/manual.pdf";s:4:"8b43";s:14:"doc/manual.sxw";s:4:"8c30";}',
+	'suggests' => array(
+	),
 );
 
 ?>
